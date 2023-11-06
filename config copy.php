e11cb1a71f240bbe32ef8debfa339e921a26df9c
EduBueno <2104423@aluno.univesp.br>
2023-11-06 20:24:44 -0300

    estou implementando e configurando a php no azure  Please enter the commit message for your changes. Lines starting

---
+++ config copy.php
@@ -0,0 +1,19 @@
+<?php
+
+$dbHost = 'localhost';
+$dbUsername = 'root';
+$dbPassword = 'paio1234';
+$dbName = 'formulario-paio';
+$dbPort = 3307;
+
+
+$conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
+
+if ($conexao->connect_errno)
+{
+    echo "Erro";
+}
+else
+{
+    echo "Conexão efetuada com sucesso";
+}
--- config.php
+++ config.php
@@ -1,23 +1,17 @@
 <?php
-
-$dbHost = 'localhost';
-$dbUsername = 'root';
-$dbPassword = 'paio1234';
-$dbName = 'formulario-paio';
-$dbPort = 3307;
-
+$dbHost = 'servidor.mysql.database.azure.com';
+$dbUsername = 'paio'; // Nome de usuário do banco de dados
+$dbPassword = 'Edu1234.';
+$dbName = 'sgbd-software-design-paio'; // Nome do banco de dados no Azure (não inclua o domínio do servidor)
+$dbPort = 3306;
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
 
-if ($conexao->connect_errno)
-{
-    echo "Erro";
-}
-else
-{
+if ($conexao->connect_errno) {
+    echo "Erro na conexão: " . $conexao->connect_error;
+    // Aqui você pode lidar com erros de conexão, se necessário.
+} else {
     echo "Conexão efetuada com sucesso";
+    // Conexão bem-sucedida, você pode continuar com suas consultas e operações no banco de dados.
 }
-
 ?>
-
-
