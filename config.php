9347b417b7341b774212226499a45ffa1be4299f
brechopaio <150359277+brechopaio@users.noreply.github.com>
2023-11-16 20:55:53 -0300

    Update config.php

--- config.php
+++ config.php
@@ -1,22 +1,17 @@
 <?php
 
 $dbHost = 'banco-dados-cadastro-clientes.mysql.database.azure.com';
-$dbUsername = 'brechopaio';
+$dbUsername = 'brechopaio@banco-dados-cadastro-clientes';
 $dbPassword = 'Edu1234.';
 $dbName = 'formulario-paio';
 $dbPort = 3306;
 
 $conexao = new mysqli($dbHost, $dbUsername, $dbPassword, $dbName, $dbPort);
 
-if ($conexao->connect_errno)
-{
+if ($conexao->connect_errno) {
     echo "Erro na conexão: " . $conexao->connect_error;
-}
-else
-{
+} else {
     echo "Conexão efetuada com sucesso";
 }
 
 ?>
-
-
